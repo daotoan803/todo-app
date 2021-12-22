1835ac52e5ec39bf8a9182a701962d1683f46a56
= <=>
2021-12-22 22:35:23 +0700

    a

--- src/controllers/authentication.controller.js
+++ src/controllers/authentication.controller.js
@@ -2,6 +2,9 @@ const User = require('../models/user.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+const ACCESS_TOKEN_KEY =
+  '89103167bc526750b32da3b7fbaa3090a06f9eb8a6bb1cfe5c61c46c667d07b571967b783b453f8ecbba753a8c50b7b5d8ea14fba7483a8d600740c6409315f9';
+
 exports.login = async (req, res) => {
   const { userId } = req;
   if (userId) return res.status(400).json({ error: 'already logged in' });
@@ -21,7 +24,7 @@ exports.login = async (req, res) => {
       username: user.username,
       exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30,
     },
-    process.env.ACCESS_TOKEN_KEY
+    ACCESS_TOKEN_KEY
   );
   res.json({ accessToken, username: user.username });
 };
@@ -30,7 +33,7 @@ exports.authenticateToken = (req, res, next) => {
   const token = req.headers['authorization']?.split(' ')[1];
   if (!token) return res.status(401).json({ error: 'Not logged in' });
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_KEY, (err, tokenObj) => {
+  jwt.verify(token, ACCESS_TOKEN_KEY, (err, tokenObj) => {
     if (err) return res.sendStatus(403);
     req.userId = tokenObj.userId;
     next();
